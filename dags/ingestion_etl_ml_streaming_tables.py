0fcf9c9ba7316e79c7ae6d83da03e80ea3762261
April Song <april.song@databricks.com>
2023-11-07 16:23:58 -0500

    update dag

--- dags/ingestion_etl_ml_streaming_tables.py
+++ dags/ingestion_etl_ml_streaming_tables.py
@@ -18,51 +18,6 @@ S3_BASE_PATH = os.environ.get("S3_BASE_PATH")
 DBT_TARGET_UC_CATALOG = os.environ.get("DBT_TARGET_UC_CATALOG")
 DBT_TARGET_UC_SCHEMA = os.environ.get("DBT_TARGET_UC_SCHEMA")
 
-autoloader_ingestion_job = {
-    "name": "databricks_ingest",
-    "email_notifications": {"no_alert_for_skipped_runs": False},
-    "webhook_notifications": {},
-    "timeout_seconds": 0,
-    "max_concurrent_runs": 1,
-    "tasks": [
-        {
-            "task_key": "databricks_ingest",
-            "run_if": "ALL_SUCCESS",
-            "notebook_task": {
-                "notebook_path": "dags/databricks/01-data-ingestion",
-                "source": "GIT"
-            },
-            "new_cluster": {
-                "spark_version": "13.2.x-cpu-ml-scala2.12",
-                "aws_attributes": {
-                    "first_on_demand": 1,
-                    "availability": "SPOT_WITH_FALLBACK",
-                    "zone_id": "us-west-2a",
-                    "spot_bid_price_percent": 100,
-                    "ebs_volume_count": 0,
-                },
-            "spark_env_vars": {
-                "DBT_TARGET_UC_CATALOG": DBT_TARGET_UC_CATALOG,
-                "DBT_TARGET_UC_SCHEMA": DBT_TARGET_UC_SCHEMA,
-                "S3_BASE_PATH": S3_BASE_PATH,
-            },
-                "node_type_id": "i3.xlarge",
-                "enable_elastic_disk": False,
-                "data_security_mode": "SINGLE_USER",
-                "runtime_engine": "STANDARD",
-                "num_workers": 1,
-            },
-            "timeout_seconds": 0,
-            "email_notifications": {},
-        }
-    ],
-    "git_source": {
-        "git_url": "https://github.com/aprilsong-db/dbt-databricks-airflow.git",
-        "git_provider": "gitHub",
-        "git_branch": "main",
-    },
-    "format": "MULTI_TASK",
-}
 
 ml_churn_pred_job = {
     "name": "churn-prediction",
